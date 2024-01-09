1253a811ead98d69b2f90418a4f12caf9c98f79a
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-01-09 15:43:33 -0300

    feat(KlineTimes): add default_intervals property

--- api/kline_utils.py
+++ api/kline_utils.py
@@ -47,3 +47,32 @@ class KlineTimes:
         self.symbol = symbol
         self.interval = interval
 
+    @property
+    def default_intervals(self):
+        """
+        Returns the list of default intervals.
+
+        Returns
+        -------
+        list of str
+            The list of default intervals.
+
+        """
+        return [
+            "1s",
+            "1m",
+            "5m",
+            "15m",
+            "30m",
+            "1h",
+            "2h",
+            "4h",
+            "6h",
+            "8h",
+            "12h",
+            "1d",
+            "3d",
+            "1w",
+            "1M",
+        ]
+
