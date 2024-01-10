0bc4e808c91a7d8db0e3367e44f8357b20ab730a
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-01-10 20:33:06 -0300

    feat: add resample_calculate_payoff function

--- dashboard/pages/home/graphs.py
+++ dashboard/pages/home/graphs.py
@@ -25,3 +25,35 @@ def resample_calculate_drawdown(
     result_df = (max_results - result_df) / max_results
     return result_df.fillna(0).astype("float32").resample(period).mean()
 
+def resample_calculate_payoff(
+    series: pd.Series,
+    method: Literal["sum", "mean"] = "sum",
+    period: str = "W"
+) -> pd.DataFrame:
+    """
+    Calculate payoff using resampling method.
+
+    Parameters:
+        series (pd.Series): The input time series data.
+        method (Literal["sum", "mean"]): The method for calculating
+        payoff.
+        (default: "sum")
+        period (str): The resampling period.
+        (default: "W" (weekly))
+
+    Returns:
+        pd.DataFrame: The calculated payoff.
+
+    """
+    rt = series.to_frame().diff().fillna(0)
+    rt_sum_pos = rt[rt > 0].resample(period).sum()
+    rt_mean_pos = rt[rt > 0].resample(period).mean()
+    rt_sum_neg = abs(rt[rt < 0]).resample(period).sum()
+    rt_mean_neg = abs(rt[rt < 0]).resample(period).mean()
+    rt = pd.DataFrame([])
+    payoff = (
+        rt_sum_pos / rt_sum_neg if method == "sum"
+        else rt_mean_pos / rt_mean_neg
+    )
+    return payoff.dropna()
+
