a94f02baa8e8a42b6ade6c1099ffef27e3994456
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-01-14 21:01:52 -0300

    feat: add lang callbacks

---
+++ dashboard/pages/lang/en_US.py
@@ -0,0 +1,125 @@
+en_US = {
+    # Get Data
+    "GET_DATA_BUTTON": "Get Data",
+    "SPOT": "Spot",
+    "FUTURES": "Futures",
+    "MARK_PRICE": "Mark Price",
+    "CUSTOM": "Custom",
+    "SYMBOL": "Symbol",
+    "TIMEFRAME": "Timeframe",
+    "UPLOAD_DATA": "Upload Data",
+    "EXCHANGE": "Exchange",
+    # General
+    "SOURCE": "Source",
+    "OPEN": "Open",
+    "HIGH": "High",
+    "LOW": "Low",
+    "CLOSE": "Close",
+    # Modify Indicators Parameters
+    "MODIFY_INDICATORS_PARAMETERS_BUTTON": "Modify Indicators Parameters",
+    "EMA_SOURCE_PRICE": "EMA Source Price",
+    "MACD_SOURCE_PRICE": "MACD Source Price",
+    "EMA_LENGTH": "EMA Length",
+    "MACD_SIGNAL_LENGTH": "MACD Signal Length",
+    "MACD_FAST_LENGTH": "MACD Fast Length",
+    "MACD_SLOW_LENGTH": "MACD Slow Length",
+    "CCI_SOURCE_PRICE": "CCI Source Price",
+    "CCI_MA_TYPE": "CCI MA Type",
+    "CCI_LENGTH": "CCI Length",
+    "CCI_CONSTANT": "CCI Constant",
+    # Modify Strategy Parameters
+    "MODIFY_STRATEGY_PARAMETERS_BUTTON": "Modify Strategy Parameters",
+    "LOWEST_LOW": "Lowest Low",
+    "TICK_SIZE": "Tick Size",
+    "PAYOFF": "Payoff",
+    "WICK_PERCENTAGE": "Wick Percentage",
+    # Modify Trend Parameters
+    "MODIFY_TREND_PARAMETERS_BUTTON": "Modify Trend Parameters",
+    "CROSSOVER_PRICE_SOURCE": "Crossover Price Source",
+    "MACD_BULLISH_VALUE": "MACD Bullish Value",
+    "CCI_BULLISH_VALUE": "CCI Bullish Value",
+    "ACTIVATE_INDICATOR": "Activate Indicator",
+    # Modify Hardware Parameters
+    "MODIFY_HARDWARE_PARAMETERS_BUTTON": "Modify Hardware Parameters",
+    "GPU_NUMBER": "GPU Quantity",
+    "CPU_CORES_NUMBER": "CPU Cores Number",
+    "GPU_WORKERS_NUMBERS": "GPU Workers",
+    "HARDWARE_TYPE": "Hardware Type",
+    # Main Button
+    "RUN_STRATEGY": "Run Strategy",
+    # Results
+    "EMPTY_RESULT": "Click Run Button to see results",
+    # Backtest
+    "MIN_LENGTH": "Min Length",
+    "MAX_LENGTH": "Max Length",
+    # Backtest Parameters
+    "MAX_EMA_LENGTH": "Max EMA Length",
+    "MIN_EMA_LENGTH": "Min EMA Length",
+    # Strategy Parameters
+    "MIN_LOWEST_LOW_LENGTH": "Min Lowest Low Length",
+    "MAX_LOWEST_LOW_LENGTH": "Max Lowest Low Length",
+    "MIN_PAYOFF": "Min Payoff",
+    "MAX_PAYOFF": "Max Payoff",
+    "MIN_WICK_PERCENTAGE": "Min Wick Percentage",
+    "MAX_WICK_PERCENTAGE": "Max Wick Percentage",
+    # Trend Parameters
+    "MIN_MACD_BULLISH_VALUE": "Min MACD Bullish Value",
+    "MAX_MACD_BULLISH_VALUE": "Max MACD Bullish Value",
+    "MIN_CCI_BULLISH_VALUE": "Min CCI Bullish Value",
+    "MAX_CCI_BULLISH_VALUE": "Max CCI Bullish Value",
+    #Result Parameters
+    "MODIFY_RESULT_PARAMETERS_BUTTON": "Modify Result Parameters",
+    "RESULT_TYPE": "Result Type",
+    "FIXED": "Fixed",
+    "NORMAL": "Normal",
+    "USE_PERCENTAGE_RESULTS": "Use Percentage Results",
+    "PROFIT": "Profit",
+    "LOSS": "Loss",
+    "RISK_FREE_RATE": "Risk Free Rate",
+    "INITIAL_CAPITAL": "Initial Capital",
+    "QUANTITY": "Quantity",
+    "PLOT_TYPE": "Plot Results",
+    "ALL_LINES": "All",
+    "ONLY_POSITIVES": "Only Positives",
+    "MODIFY_RESULT_CONFIGS_BUTTON" : "Modify Result Settings",
+    # Backtest Button
+    "RUN_BACKTEST": "Run Backtest",
+    # Presets
+    "PRESET": "Preset",
+    "COMBINATION": "Combination",
+    "RUN_MODEL": "Run Model",
+    "CANCEL_MODEL": "Cancel Model",
+    "USE_PREBUILD_MODELS": "Use Prebuild Models",
+
+    #General Components
+    "SELECT_PLACEHOLDER": "Select...",
+
+    # Indicators
+    "INDICATORS": "Indicators",
+    "MODIFY_INDICATORS": "Modify Indicators",
+    "LENGTH" : "Length",
+    "RSI": "RSI",
+    "ROLLING_RATIO_SOURCE": "Rolling Ratio Source",
+    "FIRST_LENGTH": "First Length",
+    "SECOND_LENGTH": "Second Length",
+
+    # Params
+    "MODIFY_FEAT_PARAMS": "Modify Features Params",
+    "MODIFY_MODEL_PARAMS": "Modify Model Params",
+
+    # Features
+    "FEATURES_THRESHOLD": "Features Threshold",
+    "FEATURES_SELECTION": "Features Selection",
+    "INTERMEDIATE": "Intermediate",
+
+    # Model Params
+    "MODEL_PARAMS": "Model Params",
+    "SCORING_METRIC": "Scoring Metric",
+    "GENERATE_PARAMS": "Generate Params",
+
+    # Dataset Splits
+    "MODIFY_DATASET_SPLITS": "Modify Dataset Splits",
+    "TRAIN_TEST_PERIOD": "Train / Test Split",
+    "TRAIN_TEST_RATIO": "Train / Test Ratio",
+    "VALIDATION_STARTS_AT": "Validation Starts At",
+}
---
+++ dashboard/pages/lang/pt_BR.py
@@ -0,0 +1,125 @@
+pt_BR = {
+    # Obter Dados
+    "GET_DATA_BUTTON": "Obter Dados",
+    "SPOT": "Spot",
+    "FUTURES": "Futuros",
+    "MARK_PRICE": "Preço de Marcação",
+    "CUSTOM": "Outro",
+    "SYMBOL": "Símbolo",
+    "TIMEFRAME": "Timeframe",
+    "UPLOAD_DATA": "Inserir Dados",
+    "EXCHANGE": "Corretora",
+    # Geral
+    "SOURCE": "Fonte",
+    "OPEN": "Abertura",
+    "HIGH": "Máxima",
+    "LOW": "Mínima",
+    "CLOSE": "Fechamento",
+    # Modificar Parâmetros de Indicadores
+    "MODIFY_INDICATORS_PARAMETERS_BUTTON": "Modificar Parâmetros de Indicadores",
+    "EMA_SOURCE_PRICE": "Fonte de Preços do EMA",
+    "MACD_SOURCE_PRICE": "Fonte de Preços do MACD",
+    "EMA_LENGTH": "Periodos da EMA",
+    "MACD_SIGNAL_LENGTH": "Periodos do Sinal MACD",
+    "MACD_FAST_LENGTH": "Periodos do MACD Rápido",
+    "MACD_SLOW_LENGTH": "Periodos do MACD Lento",
+    "CCI_SOURCE_PRICE": "Fonte de Preços do CCI",
+    "CCI_MA_TYPE": "Tipo de MA do CCI",
+    "CCI_LENGTH": "Periodos do CCI",
+    "CCI_CONSTANT": "Constante CCI",
+    # Modificar Parâmetros de Estratégia,
+    "MODIFY_STRATEGY_PARAMETERS_BUTTON": "Modificar Parâmetros da Estratégia",
+    "LOWEST_LOW": "Mínima Mais Baixa",
+    "TICK_SIZE": "Tamanho do Tick",
+    "PAYOFF": "Payoff",
+    "WICK_PERCENTAGE": "Porcentagem do Wick",
+    # Modificar Parâmetros de Tendência
+    "MODIFY_TREND_PARAMETERS_BUTTON": "Modificar Parâmetros da Tendência",
+    "CROSSOVER_PRICE_SOURCE": "Fonte do Preço de Cruzamento",
+    "MACD_BULLISH_VALUE": "Valor Bullish do  MACD",
+    "CCI_BULLISH_VALUE": "Valor Bullish do CCI",
+    "ACTIVATE_INDICATOR": "Ativar Indicador",
+    # Modificar Parâmetros de Hardware
+    "MODIFY_HARDWARE_PARAMETERS_BUTTON": "Modificar Parâmetros de Hardware",
+    "GPU_NUMBER": "Número de GPU",
+    "CPU_CORES_NUMBER": "Número de Cores do CPU",
+    "GPU_WORKERS_NUMBERS": "Número de workers da GPU",
+    "HARDWARE_TYPE": "Tipo de Hardware",
+    # Main Button
+    "RUN_STRATEGY": "Executar Estratégia",
+    # Results
+    "EMPTY_RESULT": "Clique no Botão Executar para ver os resultados",
+    # Backtest
+    "MIN_LENGTH": "Min Períodos",
+    "MAX_LENGTH": "Max Periodos",
+    # Parâmetros de Indicadores
+    "MAX_EMA_LENGTH": "Periodos Máximos da EMA",
+    "MIN_EMA_LENGTH": "Periodos Mínimos da EMA",
+    # Parâmetros de Estratégia
+    "MIN_LOWEST_LOW_LENGTH": "Periodos Mínimos da Mínima Mais Baixa",
+    "MAX_LOWEST_LOW_LENGTH": "Periodos Máximos da Mínima Mais Baixa",
+    "MIN_PAYOFF": "Payoff Mínimo",
+    "MAX_PAYOFF": "Payoff Máximo",
+    "MIN_WICK_PERCENTAGE": "Porcentagem Mínima do Wick",
+    "MAX_WICK_PERCENTAGE": "Porcentagem Máxima do Wick",
+    # Parâmetros de Tendência
+    "MIN_MACD_BULLISH_VALUE": "Valor Bullish Mínimo do MACD",
+    "MAX_MACD_BULLISH_VALUE": "Valor Bullish Máximo do MACD",
+    "MIN_CCI_BULLISH_VALUE": "Valor Bullish Mínimo do CCI",
+    "MAX_CCI_BULLISH_VALUE": "Valor Bullish Máximo do CCI",
+    # Parâmetros de Resultado
+    "MODIFY_RESULT_PARAMETERS_BUTTON": "Modificar Parâmetros de Resultado",
+    "RESULT_TYPE": "Tipo de Resultado",
+    "FIXED": "Fixo",
+    "NORMAL": "Normal",
+    "USE_PERCENTAGE_RESULTS": "Resultados Percentuais",
+    "PROFIT": "Lucro",
+    "LOSS": "Prejuízo",
+    "RISK_FREE_RATE": "Taxa Livre de Risco",
+    "INITIAL_CAPITAL": "Capital Inicial",
+    "QUANTITY": "Quantidade",
+    "PLOT_TYPE": "Mostrar Resultados",
+    "ALL_LINES": "Todos",
+    "ONLY_POSITIVES": "Apenas Positivos",
+    "MODIFY_RESULT_CONFIGS_BUTTON": "Modificar Configurações de Resultado",
+    # Backtest Button
+    "RUN_BACKTEST": "Rodar Backtest",
+    # Presets
+    "PRESETS": "Presets",
+    "COMBINATION": "Combinação",
+    "RUN_MODEL": "Rodar Modelo",
+    "CANCEL_MODEL": "Cancelar Modelo",
+    "USE_PREBUILD_MODELS": "Usar Modelos Feitos",
+
+    #General Components
+    "SELECT_PLACEHOLDER": "Selecionar...",
+    # Indicators
+    "INDICATORS": "Indicadores",
+    "MODIFY_INDICATORS": "Modificar Indicadores",
+    "RSI": "RSI",
+    "LENGTH" : "Periodo",
+    "ROLLING_RATIO_SOURCE": "Fonte da Proporção",
+    "FIRST_LENGTH": "Primeiro Período",
+    "SECOND_LENGTH": "Segundo Período",
+
+    # Params
+    "MODIFY_FEAT_PARAMS": "Modificar Parâmetros de Features",
+    "MODIFY_MODEL_PARAMS": "Modificar Parâmetros do Modelo",
+
+    # Features
+    "FEATURES_THRESHOLD": "Ponto de corte das Features",
+    "FEATURES_SELECTION": "Seleção de Features",
+    "INTERMEDIATE": "Intermediário",
+
+
+    # Model Params
+    "MODEL_PARAMS": "Parâmetros do Modelo",
+    "SCORING_METRIC": "Métrica de Avaliação",
+    "GENERATE_PARAMS": "Gerar Parâmetros",
+
+    # Dataset Splits
+    "MODIFY_DATASET_SPLITS": "Modificar Divisão de Dados",
+    "TRAIN_TEST_PERIOD": "Divisão de Treino / Teste",
+    "TRAIN_TEST_RATIO": "Proporção de Treino / Teste",
+    "VALIDATION_STARTS_AT": "Início da Validação",
+}
