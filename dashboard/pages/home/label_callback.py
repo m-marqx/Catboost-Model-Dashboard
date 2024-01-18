c4e1a8dc22db555bd9e6966d3f0edba4c4522d62
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-01-18 20:01:48 -0300

    feat: add DevMLLabelCallbacks class

---
+++ dashboard/pages/home/label_callback.py
@@ -0,0 +1,14 @@
+import os
+from dash import Output, Input, callback
+
+class DevMLLabelCallbacks:
+    @callback(
+        Output("dev_preset_options", "options"),
+        Input("dev_preset", "value"),
+    )
+    def update_preset_options(preset_selected):
+        environ_var = os.getenv(preset_selected)
+
+        if environ_var:
+            return environ_var.replace(" ","").split(",") + ["All"]
+        return ['None']
