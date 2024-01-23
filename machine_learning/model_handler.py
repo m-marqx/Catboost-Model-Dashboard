e8bacec0cdd25c7f2eba2cd15e3d7b48b16df023
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-01-22 23:27:52 -0300

    feat(ModelHandler): add model_returns method

--- machine_learning/model_handler.py
+++ machine_learning/model_handler.py
@@ -70,3 +70,126 @@ class ModelHandler:
         if self._has_predic_proba:
             self.y_pred_probs = estimator.predict_proba(X_test)[:, 1]
 
+    def model_returns(
+        self,
+        target_series: pd.Series,
+        fee: float = 0.1,
+        cutoff: float = 0.5,
+        step: float = 0.0,
+        long_only: bool = False,
+        short_only: bool = False,
+    ) -> tuple[pd.DataFrame, str]:
+        """
+        Calculate returns and performance metrics for a trading model.
+
+        This method calculates returns and various performance metrics
+        for a trading model using predicted probabilities and actual
+        returns. It takes into account transaction fees for trading.
+
+        Parameters:
+        -----------
+        target_series : pd.Series
+            A pandas Series containing the actual returns of the trading
+            strategy.
+        fee : float, optional
+            The transaction fee as a percentage (e.g., 0.1% for 0.1)
+            for each trade.
+            (default: 0.1)
+
+        Returns:
+        --------
+        tuple[pd.DataFrame, str]
+            A tuple containing:
+            - pd.DataFrame: A DataFrame with various columns
+            representing the trading results
+            - str: A message indicating the success of the operation
+
+        Raises:
+        -------
+        ValueError:
+            If the estimator isn't suitable for classification
+            (predict_proba isn't available).
+        """
+        if not self._has_predic_proba:
+            raise ValueError(
+                "The estimator isn't suitable for classification"
+                " (predict_proba isn't available)."
+            )
+
+        if target_series.min() > 0:
+            target_series = target_series - 1
+
+        fee = fee / 100
+        df_returns = (
+            pd.DataFrame(
+                {'y_pred_probs' : self.y_pred_probs},
+                index=self.x_test.index
+            )
+        )
+
+        target_return = target_series.reindex(df_returns.index)
+
+        df_returns["target_Return"] = target_return
+
+        if long_only:
+            df_returns["Predict"] = np.where(
+                (df_returns["y_pred_probs"] > cutoff + step), 1, 0
+            )
+        elif short_only:
+            df_returns["Predict"] = np.where(
+                (df_returns["y_pred_probs"] < cutoff - step), -1, 0
+            )
+        elif step > 0 and step is not None:
+            df_returns["Predict"] = np.where(
+                (df_returns["y_pred_probs"] > cutoff + step), 1, 0
+            )
+
+            df_returns["Predict"] = np.where(
+                (df_returns["y_pred_probs"] < cutoff - step),
+                -1, df_returns["Predict"]
+            )
+        else:
+            df_returns["Predict"] = np.where(
+                (df_returns["y_pred_probs"] > cutoff), 1, -1
+            )
+
+        df_returns["Position"] = df_returns["Predict"].shift().fillna(0)
+
+        df_returns["Result"] = (
+            df_returns["target_Return"]
+            * df_returns["Predict"]
+        )
+
+        df_returns["Liquid_Result"] = np.where(
+            (df_returns["Predict"] != 0)
+            & (df_returns["Result"].abs() != 1),
+            df_returns["Result"] - fee, 0
+        )
+
+        df_returns["Period_Return_cum"] = (
+            df_returns["target_Return"]
+        ).cumsum()
+
+        df_returns["Total_Return"] = df_returns["Result"].cumsum() + 1
+        df_returns["Liquid_Return"] = df_returns["Liquid_Result"].cumsum() + 1
+
+        df_returns["max_Liquid_Return"] = (
+            df_returns["Liquid_Return"].expanding(52).max()
+        )
+
+        df_returns["max_Liquid_Return"] = np.where(
+            df_returns["max_Liquid_Return"].diff(),
+            np.nan, df_returns["max_Liquid_Return"],
+        )
+
+        df_returns["drawdown"] = (
+            1 - df_returns["Liquid_Return"] / df_returns["max_Liquid_Return"]
+        ).fillna(0)
+
+        drawdown_positive = df_returns["drawdown"] > 0
+
+        df_returns["drawdown_duration"] = drawdown_positive.groupby(
+            (~drawdown_positive).cumsum()
+        ).cumsum()
+        return df_returns
+
