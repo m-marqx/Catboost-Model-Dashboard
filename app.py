1f370375c0f321d829f17b48ab15a4a62b5345bd
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-01-27 21:24:45 -0300

    feat: create app

---
+++ app.py
@@ -0,0 +1,13 @@
+from dashboard.layout import app
+from dashboard.pages.home.run_model import DevRunModel
+from dashboard.pages.home.label_callback import DevMLLabelCallbacks
+from dashboard.pages.home.collapse_callback import DevMLCollapseCallbacks
+
+server = app.server
+
+if __name__ == "__main__":
+    #DEV
+    DevRunModel()
+    DevMLLabelCallbacks()
+    DevMLCollapseCallbacks()
+    app.run(debug=True)
