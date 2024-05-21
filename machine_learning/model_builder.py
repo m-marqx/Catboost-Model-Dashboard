30a113c54ac16b7bbf86204174de7ffd8bc6a8ae
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-05-20 21:36:32 -0500

    feat: add calculate_model function

--- machine_learning/model_builder.py
+++ machine_learning/model_builder.py
@@ -98,3 +98,149 @@ def max_trade_adj(data_set, off_days, max_trades, pct_adj):
         )
     )
     return data_set
+
+def calculate_model(
+    dataset: pd.DataFrame,
+    feats: list,
+    test_index: int,
+    plot: bool = False,
+    output: Literal["All", "Return", "Model", "Dataset"] = "All",
+    long_only: bool = False,
+    short_only: bool = False,
+    train_in_middle: bool = False,
+    **hyperparams,
+) -> pd.DataFrame:
+    """
+    Calculates the model based on the input dataset, features, test
+    index,and hyperparameters.
+
+    Parameters
+    ----------
+    dataset : pd.DataFrame
+        The input dataset.
+    feats : list
+        The list of features.
+    test_index : int
+        The test index.
+    plot : bool, optional
+        Whether to plot the model, by default False.
+    output : Literal["All", "Return", "Model", "Dataset"], optional
+        The output type, by default "All".
+    long_only : bool, optional
+        Whether to consider long only, by default False.
+    short_only : bool, optional
+        Whether to consider short only, by default False.
+    train_in_middle : bool, optional
+        Whether to train in the middle, by default False.
+    **hyperparams
+        The hyperparameters.
+
+    Returns
+    -------
+    pd.DataFrame
+        The calculated model.
+    """
+    data_frame = klib.convert_datatypes(dataset)
+
+    train_set = data_frame.iloc[:test_index]
+    test_set = data_frame.iloc[test_index : test_index * 2]
+    validation_set = data_frame.iloc[test_index * 2 :]
+
+    features = list(feats)
+    target = ["Target_bin"]
+
+    X_train = train_set[features]
+    y_train = train_set[target]
+
+    X_test = test_set[features]
+    y_test = test_set[target]
+
+    X_validation = validation_set[features]
+    y_validation = validation_set[target]
+
+    all_x = pd.concat([X_train, X_test, X_validation])
+    all_y = pd.concat([y_train, y_test, y_validation])
+
+    if not hyperparams:
+        hyperparams = {
+            "iterations": 500,
+            "learning_rate": 0.1,
+            "eval_metric": "Logloss",
+            "random_seed": 69,
+            "logging_level": "Silent",
+            "use_best_model": True,
+        }
+
+    if train_in_middle:
+        train_pool = Pool(X_test, y_test)
+        test_pool = Pool(X_train, y_train)
+    else:
+        train_pool = Pool(X_train, y_train)
+        test_pool = Pool(X_test, y_test)
+
+    best_model = CatBoostClassifier(**hyperparams, allow_writing_files=False)
+
+    best_model.fit(train_pool, eval_set=test_pool, plot=plot)
+
+    predict = best_model.predict_proba(train_set[features])
+    cutoff = np.median(predict)
+
+    X_test = test_set[features]
+    y_test = test_set[target]
+
+    y_pred_train = np.where(best_model.predict_proba(X_train) > cutoff, 1, 0)
+
+    y_pred_test = np.where(best_model.predict_proba(X_test) > cutoff, 1, 0)
+
+    y_pred_train = pd.DataFrame(y_pred_train, index=y_train.index)[1]
+    y_pred_test = pd.DataFrame(y_pred_test, index=y_test.index)[1]
+
+    dataset_params = dict(X_test=all_x, y_test=all_y)
+
+    target_index = all_y.index
+    mh2 = ModelHandler(best_model, **dataset_params).model_returns(
+        target_series=data_frame["Target"].reindex(target_index),
+        fee=0,
+        cutoff=cutoff,
+        long_only=long_only,
+        short_only=short_only,
+    )
+
+    mh2["cuttoff"] = cutoff
+    index_splits = {
+        "train": pd.Interval(train_set.index[0], train_set.index[-1]),
+        "test": pd.Interval(test_set.index[0], test_set.index[-1]),
+        "validation": pd.Interval(validation_set.index[0], validation_set.index[-1]),
+    }
+    if output == "All":
+        return (
+            mh2,
+            best_model,
+            X_train,
+            X_test,
+            y_train,
+            y_test,
+            y_pred_train,
+            y_pred_test,
+            all_x,
+            all_y,
+            index_splits,
+        )
+    elif output == "Return":
+        return mh2, index_splits
+    elif output == "Model":
+        return best_model
+    elif output == "Dataset":
+        return (
+            X_train,
+            X_test,
+            y_train,
+            y_test,
+            y_pred_train,
+            y_pred_test,
+            all_x,
+            all_y,
+        )
+    else:
+        raise ValueError("Invalid output parameter")
+
