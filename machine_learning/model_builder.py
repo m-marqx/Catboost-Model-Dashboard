2eec5db7287fdb98f60188ee5429dd44070335e5
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-07-29 19:58:52 -0500

    refactor(model_creation): replace `MACD_clean` for `MACD_opt` feature

--- machine_learning/model_builder.py
+++ machine_learning/model_builder.py
@@ -412,23 +412,20 @@ def model_creation(
             feat_parameters["random_macd_column"],
         )
 
-
-    if "MACD_clean" in feat_parameters["random_features"]:
+    if "MACD_opt" in feat_parameters["random_features"]:
         macd_source = (
             data_frame[feat_parameters["random_source_price_macd"]]
         )
 
         data_frame = ModelFeatures(
             data_frame, test_index, feat_parameters["random_binnings_qty_macd"]
-        ).create_macd_feature(
+        ).create_macd_opt_feature(
             macd_source,
             feat_parameters["random_macd_fast_length"],
             feat_parameters["random_macd_slow_length"],
             feat_parameters["random_macd_signal_length"],
-            feat_parameters["random_macd_diff_method"],
             feat_parameters["random_macd_ma_method"],
             feat_parameters["random_macd_signal_method"],
-            feat_parameters["random_macd_column"],
         )
 
     if "TRIX" in feat_parameters["random_features"]:
