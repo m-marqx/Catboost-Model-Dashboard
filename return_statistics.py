c28514c303b57b63a0487ee4238e5465dd11083e
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-08-01 23:19:37 -0500

    refactor(calculate_all_statistics): update index settings

--- return_statistics.py
+++ return_statistics.py
@@ -128,12 +128,10 @@ class Statistics:
         stats_df["Sharpe_Ratio"] = self.calculate_estimed_sharpe_ratio()
         stats_df["Sortino_Ratio"] = self.calculate_estimed_sortino_ratio()
 
-        if self.time_span == "A":
-            stats_df["Date"] = stats_df.index.year
-        if self.time_span == "M":
-            stats_df["Date"] = stats_df.index.strftime('%m/%Y')
-        if self.time_span in ["A", "M"]:
-            stats_df = stats_df.reindex(columns=["Date"] + list(stats_df.columns[:-1]))
+        if self.time_span.endswith("YE"):
+            stats_df.index = stats_df.index.year
+        if self.time_span.endswith("ME"):
+            stats_df.index = stats_df.index.strftime('%m/%Y')
         return round(stats_df, precision)
 
     def calculate_expected_value(self):
