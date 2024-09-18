eb2b7d0648cacbdfc10bf4119d3a283b4f9083ee
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-09-17 23:03:46 -0500

    feat(BlockscoutAPI): add get_transactions method

--- api/blockscout_api.py
+++ api/blockscout_api.py
@@ -49,3 +49,62 @@ class BlockscoutAPI:
 
         self.blockscout_api_url = "https://polygon.blockscout.com/api/v2"
 
+    def get_transactions(self, txid: str, coin_name: bool = False):
+        """
+        Retrieves transaction details for a given transaction ID.
+
+        Parameters
+        ----------
+        txid : str
+            The transaction ID to retrieve details for.
+        coin_name : bool, optional
+            If True, includes the coin names in the returned dictionary
+            (default : False).
+
+        Returns
+        -------
+        dict
+            A dictionary containing the transaction details, including
+            coin totals and USD price.
+        """
+        url = f"{self.blockscout_api_url}/transactions/{txid}/token-transfers"
+
+        response = requests.get(url, params={"type": "ERC-20"}, timeout=10)
+        data = response.json()
+
+        first_coin_value = literal_eval(data["items"][0]["total"]["value"])
+        first_coin_decimals = literal_eval(
+            data["items"][0]["total"]["decimals"]
+        )
+
+        first_coin_total = first_coin_value / 10**first_coin_decimals
+
+        second_coin_value = literal_eval(data["items"][-1]["total"]["value"])
+        second_coin_decimals = literal_eval(
+            data["items"][-1]["total"]["decimals"]
+        )
+
+        second_coin_total = second_coin_value / 10**second_coin_decimals
+
+        first_coin_name = data["items"][0]["token"]["symbol"]
+        second_coin_name = data["items"][-1]["token"]["symbol"]
+
+        if first_coin_name.startswith("USD"):
+            usd_price = first_coin_total / second_coin_total
+
+        elif second_coin_name.startswith("USD"):
+            usd_price = second_coin_total / first_coin_total
+
+        else:
+            usd_price = None
+
+        if not coin_name:
+            first_coin_name = "from"
+            second_coin_name = "to"
+
+        return {
+            first_coin_name: first_coin_total,
+            second_coin_name: second_coin_total,
+            "USD Price": usd_price,
+        }
+
